ae22ed5084aa7ae3694ee50973d174e638325327
NekoMaru76 <careday17@gmail.com>
2021-04-15 20:08:23 +0700

    Update

---
+++ EndlessArrayPromise.js
@@ -0,0 +1,64 @@
+module.exports = class EndlessArrayPromise {
+    constructor() {
+        this.arrays = [];
+        this.promises = [];
+        this.index = 0;
+        this.last;
+        this.ended = false;
+
+        this.init();
+    }
+    delete() {
+        this.ended = true;
+    }
+    init() {
+        this.promises.push(new Promise((resolve, reject) => {
+            this.arrays.push({
+                resolve,
+                reject
+            });
+        }));
+    }
+    add(value) {
+        this.index++;
+
+        this.init();
+        this.arrays[this.arrays.length-2].resolve(value);
+    }
+    end(value) {
+        this.arrays[this.arrays.length-1].resolve(value);
+        this.delete();
+    }
+    error(error) {
+        this.arrays[this.arrays.length-1].reject(error);
+        this.delete();
+    }
+    [Symbol.asyncIterator]() {
+        const self = this;
+
+        return {
+            index: this.index,
+            last: false,
+            reach: [],
+            async next() {
+                const { promises, index, ended } = self;
+
+                if (ended && index <= this.index) return Promise.resolve({ value: null, done: true });
+
+                try {
+                    const promise =  promises[this.reach.length >= promises.length && !ended ? this.index : this.index++];
+                    const value = await promise;
+
+                    if (promise) this.reach.push(value);
+
+                    return Promise.resolve({
+                        value: value,
+                        done: !promise
+                    });
+                } catch (e) {
+                    Promise.reject(e);
+                }
+            }
+        }
+    }
+};
